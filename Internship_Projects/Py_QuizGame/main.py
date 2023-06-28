b3a4c546e6866ce5aeab22e908618e470be9178b
ShreyasP20 <luciferjones2015@gmail.com>
2023-06-29 02:24:44 +0530

    1_Completed

---
+++ Internship_Projects/Py_QuizGame/main.py
@@ -0,0 +1,16 @@
+print("------------------\n Get Started\n------------------ ")
+
+print("1.Which Planet is cloest to Sun?\n1.Earth \t2.Earth \n3.Earth \t4.Mercury")
+
+ch=input("Enter Your Choice")
+print(ch)
+if ch == 1:
+    print("1.Which Planet is cloest to Sun?\n1.Earth \t2.Earth \n3.Earth \t4.Mercury")
+
+    ch=input("Enter Your Choice")
+    if ch == 1:
+        print("1.Which Planet is cloest to Sun?\n1.Earth \t2.Earth \n3.Earth \t4.Mercury")
+
+        ch=input("Enter Your Choice")
+else:
+    print("Wrong Answer Bitch!")
